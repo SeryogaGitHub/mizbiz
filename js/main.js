d1a4af26440cb1ccd43f241a56475bed76a4935d
main.email.seryoga <main.email.seryoga@gmail.com>
2020-08-04 17:14:34 +0300

    start index res

--- js/main.js
+++ js/main.js
@@ -51,6 +51,90 @@ $(function () {
     slidesToScroll: 3,
     dots: true,
     nextArrow: '<button class="slick-next slick-arrow" aria-label="Next" type="button" style=""><svg width="18" height="14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M11.625 12.25L16.875 7l-5.25-5.25M1.125 7h15.75" stroke="#13AD89" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round"/></svg></button>',
-    prevArrow: '<button class="slick-prev slick-arrow" aria-label="Previous" type="button" style=""><svg width="18" height="14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M11.625 12.25L16.875 7l-5.25-5.25M1.125 7h15.75" stroke="#13AD89" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round"/></svg></button>'
+    prevArrow: '<button class="slick-prev slick-arrow" aria-label="Previous" type="button" style=""><svg width="18" height="14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M11.625 12.25L16.875 7l-5.25-5.25M1.125 7h15.75" stroke="#13AD89" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round"/></svg></button>',
+    responsive: [{
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    }, {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: false
+      }
+    }]
+  });
+  /*******************
+  *     main filter
+   ********************/
+
+  $('.reset-form').on('click', function () {
+    var $form = $(this).parents("form");
+    $form.find('.selected').text('');
+    $form.find('.default').show();
+    $form.find('.price-value .input-default').each(function () {
+      var $this = $(this);
+      var price = $this.attr("data-default");
+      $this.text(price);
+    });
+  });
+  $('.search-checked-input').on('click', '.checked-item', function () {
+    var data = "";
+    var $this = $(this);
+    var $dropdown = $this.parents('.dropdown');
+    setTimeout(function () {
+      $dropdown.find('.checked-item').each(function () {
+        var $this = $(this);
+
+        if ($this.find('input').prop("checked")) {
+          data += $this.text() + "<span>, </span>";
+        }
+      });
+      $dropdown.find('.selected').html(data);
+
+      if (data.length) {
+        $dropdown.find(".default").hide();
+      } else {
+        $dropdown.find(".default").show();
+      }
+    }, 100);
+  });
+  var $priceValue = $('.price-value');
+  $priceValue.on('keypress', 'input', function (e) {
+    var key = e.originalEvent.key;
+
+    if (!Number(key) > 0) {
+      e.preventDefault();
+    }
+  });
+  $priceValue.on('keyup', 'input', function () {
+    var $this = $(this);
+    var val = $this.val();
+    var id = $this.attr('data-id');
+
+    if (val.length === 0) {
+      $this.val(1);
+      val = 1;
+    }
+
+    $('body').find(id).text(val);
+  });
+  /****************
+  *     popup     *
+   ***************/
+
+  $('.popup').magnificPopup({
+    type: 'inline',
+    fixedContentPos: false,
+    fixedBgPos: true,
+    overflowY: 'auto',
+    closeBtnInside: true,
+    preloader: false,
+    midClick: true,
+    removalDelay: 300,
+    mainClass: 'my-mfp-zoom-in'
   });
 });
